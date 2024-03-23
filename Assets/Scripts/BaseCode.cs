0daaab590af1d35320cf84f1cbf9240acf9d88a0
nanfeng979 <306338401@qq.com>
2024-03-24 00:49:02 +0800

    实现查找周围棋子

---
+++ Assets/Scripts/BaseCode.cs
@@ -0,0 +1,22 @@
+using Y9g;
+
+public class BaseCode : Singleton<BaseCode>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum LeftOrRight
+{
+    Left,
+    Right
+}
--- Assets/Scripts/Partial/ChessBoardManager.Get.cs
+++ Assets/Scripts/Partial/ChessBoardManager.Get.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public partial class ChessBoardManager
@@ -57,4 +58,189 @@ public partial class ChessBoardManager
     {
         return chessBoardModel[row, col] != null;
     }
+
+    /// <summary>
+    /// 通过Vector2Int索引找出周围的棋子中的随机一个棋子的Vector2Int索引
+    /// </summary>
+    /// <param name="vector2IntIndex"></param>
+    /// <returns></returns>
+    private Vector2Int GetVector2IntIndexBySurroundingChessObjectWithRandom(Vector2Int vector2IntIndex)
+    {
+        List<Vector2Int> aroundCellIndex = GetSurroundingCellsByVector2IntIndex(vector2IntIndex);
+        return GetVector2IndexByChessObjectsWithRandom(aroundCellIndex);
+    }
+
+    /// <summary>
+    /// 通过Vector2Int索引获取周围格子的各自Vector2Int索引
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns></returns>
+    private List<Vector2Int> GetSurroundingCellsByVector2IntIndex(Vector2Int vector2IntIndex)
+    {
+        int rowIndex = vector2IntIndex.x;
+        int columnIndex = vector2IntIndex.y;
+
+        LeftOrRight leftOrRight = rowIndex % 2 == 0 ? LeftOrRight.Left : LeftOrRight.Right;
+
+        List<Vector2Int> aroundCellIndex = new List<Vector2Int>();
+
+        int index1OfRow = rowIndex;
+        int index1OfCol = columnIndex + 1;
+        if (index1OfRow >= 0 && index1OfRow < 8 && index1OfCol >= 0 && index1OfCol < 7)
+        {
+            aroundCellIndex.Add(new Vector2Int(index1OfRow, index1OfCol));
+        }
+        else
+        {
+            aroundCellIndex.Add(new Vector2Int(-1, -1));
+        }
+
+        if (leftOrRight == LeftOrRight.Left)
+        {
+            int index2OfRow = rowIndex + 1;
+            int index2OfCol = columnIndex;
+            if (index2OfRow >= 0 && index2OfRow < 8 && index2OfCol >= 0 && index2OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index2OfRow, index2OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        else
+        {
+            int index2OfRow = rowIndex + 1;
+            int index2OfCol = columnIndex + 1;
+            if (index2OfRow >= 0 && index2OfRow < 8 && index2OfCol >= 0 && index2OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index2OfRow, index2OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        
+        if (leftOrRight == LeftOrRight.Left)
+        {
+            int index3OfRow = rowIndex + 1;
+            int index3OfCol = columnIndex - 1;
+            if (index3OfRow >= 0 && index3OfRow < 8 && index3OfCol >= 0 && index3OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index3OfRow, index3OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        else
+        {
+            int index3OfRow = rowIndex + 1;
+            int index3OfCol = columnIndex;
+            if (index3OfRow >= 0 && index3OfRow < 8 && index3OfCol >= 0 && index3OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index3OfRow, index3OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+
+        int index4OfRow = rowIndex;
+        int index4OfCol = columnIndex - 1;
+        if (index4OfRow >= 0 && index4OfRow < 8 && index4OfCol >= 0 && index4OfCol < 7)
+        {
+            aroundCellIndex.Add(new Vector2Int(index4OfRow, index4OfCol));
+        }
+        else
+        {
+            aroundCellIndex.Add(new Vector2Int(-1, -1));
+        }
+
+        if (leftOrRight == LeftOrRight.Left)
+        {
+            int index5OfRow = rowIndex - 1;
+            int index5OfCol = columnIndex - 1;
+            if (index5OfRow >= 0 && index5OfRow < 8 && index5OfCol >= 0 && index5OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index5OfRow, index5OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        else
+        {
+            int index5OfRow = rowIndex - 1;
+            int index5OfCol = columnIndex;
+            if (index5OfRow >= 0 && index5OfRow < 8 && index5OfCol >= 0 && index5OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index5OfRow, index5OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        
+        if (leftOrRight == LeftOrRight.Left)
+        {
+            int index6OfRow = rowIndex - 1;
+            int index6OfCol = columnIndex;
+            if (index6OfRow >= 0 && index6OfRow < 8 && index6OfCol >= 0 && index6OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index6OfRow, index6OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+        else
+        {
+            int index6OfRow = rowIndex - 1;
+            int index6OfCol = columnIndex + 1;
+            if (index6OfRow >= 0 && index6OfRow < 8 && index6OfCol >= 0 && index6OfCol < 7)
+            {
+                aroundCellIndex.Add(new Vector2Int(index6OfRow, index6OfCol));
+            }
+            else
+            {
+                aroundCellIndex.Add(new Vector2Int(-1, -1));
+            }
+        }
+
+        return aroundCellIndex;
+    }
+
+    /// <summary>
+    /// 随机获取多个格子中的一个有棋子的Vector2Int索引
+    /// </summary>
+    /// <param name="cells"></param>
+    /// <returns></returns>
+    private Vector2Int GetVector2IndexByChessObjectsWithRandom(List<Vector2Int> vector2Ints)
+    {
+        List<Vector2Int> availableChess = new List<Vector2Int>();
+        foreach (var vector2Int in vector2Ints)
+        {
+            // 如果格子存在，且格子上有棋子
+            if (vector2Int.x != -1 && vector2Int.y != -1 && IsExistChessOnCellByRowColIndex(vector2Int.x, vector2Int.y))
+            {
+                availableChess.Add(vector2Int);
+            }
+        }
+
+        if (availableChess.Count == 0)
+        {
+            return new Vector2Int(-1, -1);
+        }
+
+        return availableChess[Random.Range(0, availableChess.Count)];
+    }
+
 }
