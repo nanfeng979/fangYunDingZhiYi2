cd3975599f7d6e36a105f249e727a1d2309c73a4
nanfeng979 <306338401@qq.com>
2024-04-13 22:55:28 +0800

    优化代码

--- Assets/Scripts/TiesManager.cs
+++ Assets/Scripts/TiesManager.cs
@@ -15,9 +15,9 @@ public class TiesManager : Singleton<TiesManager>
     }
 
     private GameObject[] tieObjectList = new GameObject[8]; // 羁绊的显示对象
-    private string[] tiesNameArray = new string[8]; // 按顺序登记羁绊名字
-    private Dictionary<string, int> tiesStatusDic = new Dictionary<string, int>(); // 记录羁绊是否激活
+    private Dictionary<string, bool> tiesStatusDic = new Dictionary<string, bool>(); // 记录羁绊是否激活
     
+    private List<TiesUI> tiesUIList = new List<TiesUI>(); // 羁绊UI列表
 
     private void Start() {
         InitTiesStatusDic(); // 初始化羁绊状态字典
@@ -50,6 +50,14 @@ public class TiesManager : Singleton<TiesManager>
         if (Input.GetKeyDown(KeyCode.Keypad6)) {
             ActiveTie(new List<string> { "Juedoudashi" });
         }
+
+        if (Input.GetKeyDown(KeyCode.Keypad7)) {
+            ActiveTie(new List<string> { "Jianzhi" });
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad8)) {
+            UnActiveTie("Jianzhi");
+        }
     }
 
     /// <summary>
@@ -57,12 +65,13 @@ public class TiesManager : Singleton<TiesManager>
     /// </summary>
     private void InitTiesStatusDic()
     {
-        tiesStatusDic.Add("Huwei", 0);
-        tiesStatusDic.Add("Jushen", 0);
-        tiesStatusDic.Add("Mozhiying", 0);
-        tiesStatusDic.Add("Qingtianwei", 0);
-        tiesStatusDic.Add("Youhun", 0);
-        tiesStatusDic.Add("Juedoudashi", 0);
+        tiesStatusDic.Add("Huwei", false);
+        tiesStatusDic.Add("Jushen", false);
+        tiesStatusDic.Add("Mozhiying", false);
+        tiesStatusDic.Add("Qingtianwei", false);
+        tiesStatusDic.Add("Youhun", false);
+        tiesStatusDic.Add("Juedoudashi", false);
+        tiesStatusDic.Add("Jianzhi", false);
     }
 
     /// <summary>
@@ -71,52 +80,60 @@ public class TiesManager : Singleton<TiesManager>
     private void LoadTiesList() {
         for (int i = 0; i < tiesListGameObject.transform.childCount; i++) {
             tieObjectList[i] = tiesListGameObject.transform.GetChild(i).gameObject;
-            tiesNameArray[i] = "";
         }
     }
 
+    /// <summary>
+    /// 激活羁绊
+    /// </summary>
+    /// <param name="tiesList"></param>
     public void ActiveTie(List<string> tiesList) {
         for (int i = 0; i < tiesList.Count; i++) {
             // 如果羁绊没有被激活
-            if (tiesStatusDic[tiesList[i]] == 0)
+            if (tiesStatusDic[tiesList[i]] == false)
             {
-                // 羁绊激活的位置
-                // int indexOfTiesList = 0;
-                // 激活羁绊
-                // ActiveOneTieUI(tiesList[i], i);
                 ActiveTieFirstLogic(tiesList[i]);
             }
             // 如果羁绊已经被激活
             else
             {
-                // 找到羁绊的位置，增加羁绊数量
+                // 找到羁绊的排序位置，增加羁绊数量
                 tiesUIList.Find((tiesUI) => {
                     if (tiesUI.tieName == tiesList[i])
                     {
                         tiesUI.TieCount += 1;
-                        Debug.Log(tiesUI.TieCount);
                         return true;
                     }
                     return false;
                 });
             }
-
-            // 激活羁绊，逻辑层
-            // ActiveTieLogic(tiesList[i]);
         }
 
         SortTiesUIList(); // 排序羁绊UI列表 // TODO: 后面要减少排序次数
         UpdateTiesUI(); // 更新羁绊UI // TODO: 后面要减少刷新次数
     }
 
-
     /// <summary>
-    /// 激活羁绊，逻辑层
+    /// 取消激活羁绊
     /// </summary>
     /// <param name="tieName"></param>
-    private void ActiveTieLogic(string tieName)
-    {
-        tiesStatusDic[tieName] += 1; // 激活羁绊
+    public void UnActiveTie(string tieName) {
+        tiesUIList.Find((tiesUI) => {
+            if (tiesUI.tieName == tieName)
+            {
+                tiesUI.TieCount -= 1;
+                if (tiesUI.TieCount == 0)
+                {
+                    tiesStatusDic[tiesUI.tieName] = false;
+                    tiesUIList.Remove(tiesUI);
+                }
+                return true;
+            }
+            return false;
+        });
+
+        SortTiesUIList(); // 排序羁绊UI列表
+        UpdateTiesUI(); // 更新羁绊UI
     }
 
     /// <summary>
@@ -125,75 +142,66 @@ public class TiesManager : Singleton<TiesManager>
     /// <param name="tieName"></param>
     private void ActiveTieFirstLogic(string tieName)
     {
-        tiesStatusDic[tieName] = 1; // 激活羁绊
+        tiesStatusDic[tieName] = true; // 激活羁绊
 
-        tiesUIList.Add(TiesUISerializableFactory.CreateTiesUI(tieName));
+        tiesUIList.Add(TiesUISerializableFactory.CreateTiesUI(tieName)); // 在羁绊UI列表中添加羁绊
     }
 
     /// <summary>
     /// 更新羁绊UI
     /// </summary>
-    /// TODO: 目前是整体刷新，后续可能会有其他刷新方式
     private void UpdateTiesUI()
     {
-        for (int i = 0; i < tiesUIList.Count; i++)
+        for (int i = 0; i < tieObjectList.Length; i++)
         {
-            GameObject tieObject = tieObjectList[i];
-            if (tiesUIList[i].TieSprite != null)
-            {
-                tieObject.GetComponent<Image>().sprite = tiesUIList[i].TieSprite;
-            }
-            tieObject.GetComponent<Image>().enabled = true; // 激活羁绊UI的Image组件
+            GameObject tieObject = tieObjectList[i]; // 获取羁绊UI对象
+            Image tieObjectImage = tieObject.GetComponent<Image>(); // 获取羁绊UI的Image组件
+            Text tieObjectText = tieObject.transform.GetChild(0).GetComponent<Text>(); // 获取羁绊UI的Text组件
 
-            Text text = tieObject.transform.GetChild(0).GetComponent<Text>();
-            text.enabled = true;
-            text.text = tiesUIList[i].TieCount.ToString();
+            // 如果羁绊UI列表中有羁绊
+            if (i < tiesUIList.Count)
+            {
+                tieObjectImage.enabled = true; // 激活羁绊UI的Image组件
+                tieObjectText.enabled = true; // 激活羁绊UI的Text组件
+
+                tieObjectImage.sprite = tiesUIList[i].TieSprite; // 更新羁绊图片
+                tieObjectText.text = tiesUIList[i].TieCount.ToString(); // 更新羁绊数量
+            }
+            // 如果羁绊UI列表中没有羁绊
+            else
+            {
+                tieObjectImage.enabled = false; // 关闭羁绊UI的Image组件
+                tieObjectText.enabled = false; // 关闭羁绊UI的Text组件
+            }
         }
     }
 
-    /// <summary>
-    /// 激活羁绊，UI层
-    /// </summary>
-    /// <param name="tieName"></param>
-    /// <param name="indexOfTiesList"></param>
-    private void ActiveOneTieUI(string tieName, int indexOfTiesList)
-    {
-        GameObject tieObject = tieObjectList[indexOfTiesList];
-        Sprite tieSprite = TieSpriteMap.Instance.GetSprite(tieName, 0);
-        tieObject.GetComponent<Image>().sprite = tieSprite;
-        tieObject.GetComponent<Image>().enabled = true; // 激活羁绊UI的Image组件
-    }
-
-
-    //////////////////////////
-    private List<TiesUI> tiesUIList = new List<TiesUI>();
 
     /// <summary>
     /// 排序羁绊UI列表
     /// </summary>
-    /// TODO: 目前是按照优先级排序，后续可能会有其他排序方式
     private void SortTiesUIList()
     {
         tiesUIList.Sort((a, b) => {
             if (a.TiesUIPriority < b.TiesUIPriority)
             {
-                // if (a.tieCount > b.tieCount)
-                // {
-                //     return -1;
-                // }
-                // else if (a.tieCount < b.tieCount)
-                // {
-                //     return 1;
-                // }
-                // else
-                // {
-                //     return 0;
-                // }
                 return 1;
             }
-            else if (a.TiesUIPriority > b.TiesUIPriority)
+            else if (a.TiesUIPriority >= b.TiesUIPriority)
             {
-                return -1;
+                if (a.TieCount >= b.TieCount)
+                {
+                    return -1;
+                }
+                else if (a.TieCount < b.TieCount)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+                // return -1;
             }
             else
             {
@@ -256,6 +264,7 @@ public class TiesUI
             Sprite tieSpriteTemp = TieSpriteMap.Instance.GetSprite(tieName, (int)tiesUIPriority);
             if (tieSpriteTemp != null)
             {
+                tieSprite = tieSpriteTemp;
                 return tieSpriteTemp;
             }
 
@@ -288,12 +297,12 @@ public class TiesUI
 
 public enum TiesUIPriority
 {
-    gray,
-    copper,
-    silvery,
+    gray = 0,
+    copper = 1,
+    silvery = 2,
     // red,
-    glod,
-    diamond,
+    glod = 3,
+    diamond = 4,
 }
 
 public class TiesUISerializableFactory
@@ -324,6 +333,10 @@ public class TiesUISerializableFactory
                 };
             case "Juedoudashi":
                 return new TiesUI("Juedoudashi")
+                {
+                };
+            case "Jianzhi":
+                return new TiesUI("Jianzhi", new List<int> { 0, 3, 5, 7, 10 })
                 {
                 };
             default:
